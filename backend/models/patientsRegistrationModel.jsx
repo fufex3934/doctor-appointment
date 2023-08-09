59ac7927a83eeba3c944f8b7b11beea46fb8743f
Fufa Wakgari <fufawakgari174@gmail.com>
2023-08-09 17:53:14 +0300

    correction for login page

--- backend/models/patientsRegistrationModel.jsx
+++ backend/models/patientsRegistrationModel.jsx
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 
 const patientSchema = mongoose.Schema({
-<<<<<<< HEAD
-    fullName:{type:String,required:true},
-    email:{type:String,required:true},
-    birthDate:{type:Date,required:true},
-    password:{type:String,required:true},
-=======
     _id: mongoose.Types.ObjectId,
     fullName: { type: String, required: true },
     email: { type: String, required: true },
@@ -14,7 +8,6 @@ const patientSchema = mongoose.Schema({
     password: { type: String, required: true },
 }, {
     collection: "Patient"
->>>>>>> 9241e799493a8c5a8fd685c97d0629e02583d124
 });
 
 module.exports = mongoose.model('Patient', patientSchema);
