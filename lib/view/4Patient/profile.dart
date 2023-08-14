9e26379ee9f975ce58b586b81685d01e96570acf
Fufa Wakgari <fufawakgari174@gmail.com>
2023-08-14 09:37:50 +0300

    adding patient profile page

--- lib/view/4Patient/profile.dart
+++ lib/view/4Patient/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/cli_commands.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -12,23 +13,21 @@ class PatientProfile extends StatefulWidget {
 class _PatientProfileState extends State<PatientProfile> {
   File? _image;
 
-
-
   Future<void> _getImageFromCamera() async {
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(source: ImageSource.camera);
 
-    _updateProfileImage(pickedImage as PickedFile?);
+    _updateProfileImage(pickedImage);
   }
 
   Future<void> _getImageFromGallery() async {
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(source: ImageSource.gallery);
 
-    _updateProfileImage(pickedImage as PickedFile?);
+    _updateProfileImage(pickedImage);
   }
 
-  void _updateProfileImage(PickedFile? pickedImage) {
+  void _updateProfileImage(XFile? pickedImage) {
     setState(() {
       if (pickedImage != null) {
         _image = File(pickedImage.path);
@@ -38,30 +37,50 @@ class _PatientProfileState extends State<PatientProfile> {
 
   // ... other methods ...
 
+  List<Map<String, String>> patientInfo = [
+    {"Age": "38"},
+    {"Gender": "M"},
+    {"DOB": "Jan 18 1982"},
+    {"Fathers Name :": "aaaa"},
+    {"Mothers Name ": "bbb"},
+    {"Blood Type ": "A+"},
+    {"Weight": "72"},
+    {"Height": "55'"},
+    {"Alergy": "cccc"}
+  ];
+
+  List<Map<String, String>> addressInfo = [
+    {"": "1168 pw parners rd, portland,USA"},
+    {"Phone": "384889977746"},
+    {"Alt Phone": "4567890983"},
+    {"Email": "fufa@gmail.com"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.only(left: 10,right: 10,top: 40),
           child: Column(
             children: [
               Container(
                 child: Row(
                   children: [
                     Stack(
-                      alignment: Alignment.bottomLeft,
+                      alignment: Alignment.bottomRight,
                       children: [
                         Container(
-                          width: 200,
-                          height: 200,
+                          width: 100,
+                          height: 100,
                           decoration: BoxDecoration(
-                            shape: BoxShape.circle,
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20),
                             image: _image != null
                                 ? DecorationImage(
-                              image: FileImage(_image!),
-                              fit: BoxFit.cover,
-                            )
+                                    image: FileImage(_image!),
+                                    fit: BoxFit.cover,
+                                  )
                                 : null,
                           ),
                         ),
@@ -95,7 +114,7 @@ class _PatientProfileState extends State<PatientProfile> {
                             );
                           },
                           child: Container(
-                            margin: EdgeInsets.all(16),
+                            margin: EdgeInsets.only(top: 16),
                             padding: EdgeInsets.all(8),
                             decoration: BoxDecoration(
                               color: Colors.blue,
@@ -109,10 +128,170 @@ class _PatientProfileState extends State<PatientProfile> {
                         ),
                       ],
                     ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "patient name".capitalize(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Patient Id 12467577".toUpperCase(),
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w300),
+                        ),
+                        Text(
+                          "since oct 7,2023".toUpperCase(),
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 70,
+                    ),
+                    Icon(Icons.edit)
                   ],
                 ),
               ),
-              // ... other profile details and editing functionality ...
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.text_snippet,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Patient Info".capitalize(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            SizedBox(
+                              width: 160,
+                            ),
+                            Icon(Icons.edit)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            for (var info in patientInfo)
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    info.keys.first,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    info.values.first,
+                                    style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: 20),
+                                ],
+                              ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.home_sharp),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Address".capitalize(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            SizedBox(
+                              width: 170,
+                            ),
+                            Icon(Icons.edit)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            for (var info in addressInfo)
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    info.keys.first,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text(
+                                    info.values.first,
+                                    style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: 20),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
