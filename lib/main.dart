2131793dfb22a20a6f12c9317f1a053acb837ba3
Fufa Wakgari <fufawakgari174@gmail.com>
2023-08-15 23:51:12 +0300

    firebase setup for chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:doctor/view/4Patient/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'controller/Provider.dart';
 import 'view/splashPage/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     ChangeNotifierProvider(
       create: (context) => PatientProvider(),
